972a2f7f5bd84b3a1c589a95de07a487f669b651
ToOrtner <tortner@dc.uba.ar>
2020-04-23 12:04:57 -0300

    agregue casos de test

--- tp1/src/c++/tests.cpp
+++ tp1/src/c++/tests.cpp
@@ -8,15 +8,57 @@ TEST(funciones, prueba1){
               {5, 2, 1}};
   matriz a = elimGauss(n);
 
-  for (int i = 0; i < a.size(); ++i) {
+  for (int i = 0; i < a.size(); ++i) { // imprimo lo que da la funcion
     cout << endl;
     for (int j = 0; j < a[0].size(); ++j) {
       cout << a[i][j] << "  ";
 
     }
   }
-  matriz res = {{1, 2, 1},
+  matriz res = {{1, 2, 1},  // resultado que deberia dar
                 {0, -4, -3},
                 {0, 0, 2}};
   EXPECT_EQ(a, res);
 }
+
+TEST(funciones, prueba2){
+
+  matriz m = {{1, 2, 0}, {1, 3, 6}, {1, 7, 30}};
+
+  matriz a = elimGauss(m);
+
+  for (int i = 0; i < a.size(); ++i) { // imprimo lo que da la funcion
+    cout << endl;
+    for (int j = 0; j < a[0].size(); ++j) {
+      cout << a[i][j] << "  ";
+
+    }
+  }
+  matriz res = {{1, 2, 0},  // resultado que deberia dar
+                {0, 1, 6},
+                {0, 0, 0}};
+  EXPECT_EQ(a, res);
+}
+
+
+TEST(funciones, pruebaNoCuadrada){
+
+  matriz m = {{1, 2, 3, 4},
+              {5, 6, 3, 2},
+              {3, 2, 1, 5}};
+
+  matriz a = elimGauss(m);
+
+  for (int i = 0; i < a.size(); ++i) { // imprimo lo que da la funcion
+    cout << endl;
+    for (int j = 0; j < a[0].size(); ++j) {
+      cout << a[i][j] << "  ";
+
+    }
+  }
+  matriz res = {{1, 2, 3, 4},  // resultado que deberia dar
+                {0, -4, -12, -18},
+                {0, 0, 4, 11}};
+  EXPECT_EQ(a, res);
+}
+
