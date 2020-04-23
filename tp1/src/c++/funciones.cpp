16f89b9b54ea7317cab8d321a7a0b51bfff84e00
ToOrtner <tortner@dc.uba.ar>
2020-04-23 13:22:19 -0300

    las matrices son de doubles
    los tests dan error por culpa del redondeo

--- tp1/src/c++/funciones.cpp
+++ tp1/src/c++/funciones.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-#define matriz vector<vector<float>>
+#define matriz vector<vector<double>>
+#define nat unsigned int
 
 matriz elimGauss (matriz &M){
   matriz res = M;
@@ -12,14 +13,14 @@ matriz elimGauss (matriz &M){
                                                                               // (asi la primer celda vale 1)
   // todo:  queremos que la diagonal tenga unos? o con triangular alcanza?
 
-  unsigned int columna=0;
-  unsigned int fila=1;
-  for (int k = 0; k < res.size()-1; ++k) {  // fila pivot
+  nat columna=0;
+  nat fila=1;      //tengo que empezar a restar desde la 2da fila
+  for (nat k = 0; k < res.size()-1; ++k) {  // fila pivot
 
-    for (int i = fila; i < res.size(); ++i) {  // fila actual
-      float x = res[i][columna] / res[k][columna];              //cantidad de veces a restar esa fila
+    for (nat i = fila; i < res.size(); ++i) {  // fila actual en la que estoy restando
+      double x = res[i][columna] / res[k][columna];              //cantidad de veces a restar esa fila
 
-      for (int j = columna; j < res[0].size(); ++j) {  //columna actual
+      for (nat j = columna; j < res[0].size(); ++j) {  //columna actual
         res[i][j] -= x * res[k][j];
       }
     }
@@ -28,20 +29,3 @@ matriz elimGauss (matriz &M){
   }
   return res;
 }
-/*
-int main() {
-  matriz m = {{1, 2, 0}, {1, 3, 6}, {1, 7, 30}};
-
-  matriz n = {{1, 2, 1}, {3, 2, 0}, {5, 2, 1}};
-  matriz a = elimGauss(n);
-  for (int i = 0; i < a.size(); ++i) {
-    cout << endl;
-    for (int j = 0; j < a[0].size(); ++j) {
-      cout << a[i][j] << "  " ;
-
-    }
-  }
-
-  return 0;
-}
-*/
--- tp1/src/c++/tests.cpp
+++ tp1/src/c++/tests.cpp
@@ -62,3 +62,48 @@ TEST(funciones, pruebaNoCuadrada){
   EXPECT_EQ(a, res);
 }
 
+TEST(funciones, pruebaNoCuadrada2){
+
+  matriz m = {{5, 1, 3},
+              {2, 1, 3},
+              {4, 2, 3},
+              {2, 3, 2}};
+
+  matriz a = elimGauss(m);
+
+  for (int i = 0; i < a.size(); ++i) { // imprimo lo que da la funcion
+    cout << endl;
+    for (int j = 0; j < a[0].size(); ++j) {
+      cout << a[i][j] << "  ";
+
+    }
+  }
+  matriz res = {{5, 1, 3},  // resultado que deberia dar
+                {0, 3.0/5.0, 9.0/5.0},
+                {0, 0, -3},
+                {0, 0, 0}};
+  EXPECT_EQ(a, res);
+}
+
+TEST(funciones, decYneg){   //por pantalla imprime los mismos resultados, pero el Expect-EQ da error
+
+  matriz m = {{-1.5, 1.2, 2.2},
+              {5.1, -12.2, 1.1},
+              {-2.2, 31.1, 1.2},
+              {0.2, -5.1, 5.1}};
+
+  matriz a = elimGauss(m);
+
+  for (int i = 0; i < a.size(); ++i) { // imprimo lo que da la funcion
+    cout << endl;
+    for (int j = 0; j < a[0].size(); ++j) {
+      cout << a[i][j] << "  ";
+
+    }
+  }
+  matriz res = {{-1.5, 1.2, 2.2},  // resultado que deberia dar
+                {0, -203.0/25, 429.0/50},
+                {0, 0, 352921.0/12180.0},
+                {0, 0, 0}};
+  EXPECT_EQ(a, res);
+}
