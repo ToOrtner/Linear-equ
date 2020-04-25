5277b104fc0565a55e0d3527f219e42f7e6bdbca
dsenarruzza <diegosenarruzza@gmail.com>
2020-04-25 18:42:44 -0300

    Fix solveSystem

--- tp1/src/c++/funciones.cpp
+++ tp1/src/c++/funciones.cpp
@@ -49,15 +49,15 @@ vector<ranking_t> solveSystem(matrix &C, vector<ranking_t> &b) {
   //Creo un vector de resultados
   vector<ranking_t> result = vector<ranking_t>(C.size(), 0);
 
-  for (int fila = C.size() - 1; fila >= 0; ++fila) {
+  for (int fila = C.size() - 1; fila >= 0; --fila) {
     //Suma el elemento en la expansion de la matriz
     result[fila] += b[fila];
     //Resta todos los elementos de fila a la derecha de la diagonal
-    for (int columna = C.size() - 1; columna > fila; ++columna) {
-      result[fila] -= C[fila][columna] * result[fila];
+    for (int columna = C.size() - 1; columna > fila; --columna) {
+      result[fila] -= C[fila][columna] * result[columna];
     }
     //Divide por el valor de la diagonal en esa fila.
-    result[fila] /= C[fila][fila];
+    result[fila] = (double)(result[fila] / C[fila][fila]);
   }
 
   return result;
