b567d2fe851897746c9b3cb7814a0fc51d700c66
AWolfsdorf <alexiswolfsdorf@gmail.com>
2020-04-25 16:14:01 -0300

    b por referencia

--- tp1/src/c++/funciones.cpp
+++ tp1/src/c++/funciones.cpp
@@ -5,14 +5,15 @@
 #include <vector>
 #include "defines.h"
 
-void elimGaussExpandida(matrix &C, vector<ranking_t> b);
+void elimGaussExpandida(matrix &C, vector<ranking_t> &b);
 void elimGauss(matrix &C);
 
 void elimGauss(matrix &C){
-    elimGaussExpandida(C, vector<ranking_t>());
+  vector<ranking_t> b = vector<ranking_t>();
+  elimGaussExpandida(C, b);
 }
 
-void elimGaussExpandida(matrix &C, vector<ranking_t> b){
+void elimGaussExpandida(matrix &C, vector<ranking_t> &b){
 
   bool isExpanded = !b.empty();
 
@@ -21,10 +22,8 @@ void elimGaussExpandida(matrix &C, vector<ranking_t> b){
 
   //Primero triangulo
   for (nat fila_pivot = 0; fila_pivot < C.size() - 1; ++fila_pivot) {  // fila pivot
-
     for (nat fila_actual = fila; fila_actual < C.size(); ++fila_actual) {  // fila actual en la que estoy restando
       ranking_t multiplicador = C[fila_actual][columna] / C[fila_pivot][columna];              //cantidad de veces a restar esa fila
-
 
       for (nat columna_actual = columna; columna_actual < C[0].size(); ++columna_actual) {  //columna actual
         C[fila_actual][columna_actual] -= multiplicador * C[fila_pivot][columna_actual];
