0f22fce73d73f02a7a219cedcd52f14d83d3a23f
ToOrtner <tortner@dc.uba.ar>
2020-04-29 22:33:27 -0300

    agrego WP y Laplace a experimentos.cpp

--- tp1/testsC/experimentos.cpp
+++ tp1/testsC/experimentos.cpp
@@ -10,15 +10,18 @@
 
 static const string FILE_OUTPUT_DELTA_PREFIX =  "exps/output_";
 void correrCMM();
-
+void correrWP();
+void correrLaplace();
 int main() {
   correrCMM();
+  correrWP();
+  correrLaplace();
   return 1;
 }
 
 void correrCMM() {
   Season season = Season::parseDat("data/atp_matches_2015.dat");
-  string file_name = FILE_OUTPUT_DELTA_PREFIX + to_string(season.getCantPartidos()) + "_" + to_string(season.getCantEquipos()) + ".csv";
+  string file_name = FILE_OUTPUT_DELTA_PREFIX + "_CMM_" + to_string(season.getCantPartidos()) + "_" + to_string(season.getCantEquipos()) + ".csv";
 
   //Output file
   ofstream outputFile;
@@ -28,6 +31,47 @@ void correrCMM() {
   //Contabilizo el tiempo por las dudas
   auto start = chrono::steady_clock::now();
   season.calculateCMMRanking();
+  auto end = chrono::steady_clock::now();
+
+  auto timeElapsed = end - start;
+  outputFile << chrono::duration <double, milli>(timeElapsed).count() << endl;
+
+  outputFile.close();
+}
+
+void correrWP() {
+  Season season = Season::parseDat("data/atp_matches_2015.dat");
+  string file_name = FILE_OUTPUT_DELTA_PREFIX + "_WP_" + to_string(season.getCantPartidos()) + "_" + to_string(season.getCantEquipos()) + ".csv";
+
+  //Output file
+  ofstream outputFile;
+  outputFile.open (file_name, fstream::in | fstream::out | fstream::trunc);
+  outputFile << "tiempo" << endl;
+
+  //Contabilizo el tiempo por las dudas
+  auto start = chrono::steady_clock::now();
+  season.calculateWPRanking();
+  auto end = chrono::steady_clock::now();
+
+  auto timeElapsed = end - start;
+  outputFile << chrono::duration <double, milli>(timeElapsed).count() << endl;
+
+  outputFile.close();
+}
+
+
+void correrLaplace() {
+  Season season = Season::parseDat("data/atp_matches_2015.dat");
+  string file_name = FILE_OUTPUT_DELTA_PREFIX + "_Laplace_" + to_string(season.getCantPartidos()) + "_" + to_string(season.getCantEquipos()) + ".csv";
+
+  //Output file
+  ofstream outputFile;
+  outputFile.open (file_name, fstream::in | fstream::out | fstream::trunc);
+  outputFile << "tiempo" << endl;
+
+  //Contabilizo el tiempo por las dudas
+  auto start = chrono::steady_clock::now();
+  season.calculateWPRanking();
   auto end = chrono::steady_clock::now();
 
   auto timeElapsed = end - start;
