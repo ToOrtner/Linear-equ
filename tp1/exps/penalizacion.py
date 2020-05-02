126ea81463ac097ae52e424e41615aa139b3362f
ToOrtner <tortner@dc.uba.ar>
2020-05-02 20:58:13 -0300

    Coeficiente acierto

--- tp1/exps/penalizacion.py
+++ tp1/exps/penalizacion.py
@@ -8,14 +8,13 @@ with open(file,'r') as csvfile,open(out,'w') as outfile:
     score_reader = csv.reader(csvfile,delimiter=',',skipinitialspace=True)
 
     for partido in score_reader:
-        dif = abs(patido[4] - partido[5])
-        if (partido[6] == 'Si'):
-        out.write(dif)
-        elif :
-            out.write(-1 *dif)
-        endif
+        resta = float(partido[4]) - float(partido[3])
+        dif = abs(resta)
+        if (partido[5] == 'Si'):
+            outfile.write(str(dif) + '\n')
+        else: outfile.write(str(-1 *dif) + '\n')
         
-    
+print ('done')    
 
 
 
